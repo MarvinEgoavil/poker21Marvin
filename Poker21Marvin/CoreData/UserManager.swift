f84f61b46dc98257f83e83b55cfa77d222fe4428
Marvin <Marvinegoavilz@gmail.com>
2020-11-20 06:10:35 +0000

    add comenta

--- Poker21Marvin/CoreData/UserManager.swift
+++ Poker21Marvin/CoreData/UserManager.swift
@@ -9,22 +9,30 @@ import Foundation
 import CoreData
 
 ///Clase User CORE DATA
+
+// Creamos la clase UserManager, esta clase la usamos cuando guardamos un ususario y cuando listamos los usuarios
 class UserManager {
     
+    // Hace instancia estatica del userManager
     static let sharedManager = UserManager()
     
+    // Se pone este constructor vacio, para que instancia una sola vez
     private init(){}
+    
+    //Trae el contaxto del core data
     
     let viewContext = AppDelegate().persistentContainer.viewContext
     
-    //MARK: SAVE USER
+    // Crea una funcion que guarda el usuario
     func saveUser(name: String, score: Int, isWinner: Bool, completion: @escaping (_ result: String?) -> ()) {
-        
+        // creamos una instancia del usuario y le pasamos el contexto
         let newUser = User(context: viewContext)
+        
+        // El usuario lo lleno con los parametros
         newUser.isWinner = isWinner
         newUser.name = name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? "Sin nombre" : name
         newUser.score = Int32(score)
-        
+        //Aplicamos el bloque Do cath para manejar cualquie excepcion
         do {
             try viewContext.save()
             completion(nil)
@@ -32,7 +40,7 @@ class UserManager {
             completion(error.localizedDescription)
         }
     }
-    
+    // Creamos una variable que nos devuelve la lista de usuarios que esta guardado en coredata
     var listUsers: [User]? {
         let request: NSFetchRequest<User> = User.fetchRequest()
         let users = try! viewContext.fetch(request)
--- Poker21Marvin/ScoreViewController.swift
+++ Poker21Marvin/ScoreViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+// Es el viewControler  donde se refleja los puntajes, este es un View Controller, dentro ponemos uhn tableviewController para listar los usuarios con sus puntajes respectivos
 class ScoreViewController: UIViewController {
 
+    //Hago una referencia del Storyboard al view controlller
     @IBOutlet weak var tbScore: UITableView!
 
-    fileprivate var userList = [User]()
+    // Creamos una lista de tipo user del modelo de coredata
+     var userList = [User]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,24 +24,29 @@ class ScoreViewController: UIViewController {
         tbScore.dataSource = self
         tbScore.register(UINib(nibName: "ScoreTableViewCell", bundle: nil), forCellReuseIdentifier: "ScoreTableViewCell")
         
+        //Asignamos la lista de usuarios del coredata a la variable userlist, en caso sea nula que sea vacio
         userList = UserManager.sharedManager.listUsers ?? []
         
     }
 
 }
 
+// Hacemos una extension  para incluir los delegados del tableViewcontroller
 extension ScoreViewController: UITableViewDelegate, UITableViewDataSource {
-    
+    // asiganmos el numero o cantidad de items de la lista
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userList.count
     }
     
+    // creamos la variable loCell, loCell y vuelve un objetos de de celda de vista de tabla reutilizable ubicado por su identificador
+    
+    // usamos el guard para validar la celda en caso sea nulo no va a correr el codigo y retorna un  UITableViewCell vacio
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let loCell = tbScore.dequeueReusableCell(withIdentifier: "ScoreTableViewCell", for: indexPath) as? ScoreTableViewCell else {
             return UITableViewCell()
         }
-        
+        // llena los datos a la celda
         let user = userList[indexPath.row]
         loCell.setData(isWinner: user.isWinner, name: user.name ?? "", score: Int(user.score))
         return loCell
